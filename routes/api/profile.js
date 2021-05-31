b265de90dced595a33db589986e3cb9d7965ff99
gnanesh05 <gnanesh007.mk@gmail.com>
2021-05-31 14:29:45 +0530

    create/update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,111 @@
 const express = require('express');
 const router  = express.Router();
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
+const auth = require('../../middleware/auth');
+const {check, validationResult} = require('express-validator');
 
-//test route
-router.get('/',(req,res)=>{
+//profile route
+router.get('/me', auth ,async (req,res)=>{
+
+    try{
+     const profile = await Profile.findOne({user: req.user.id}).populate('user',['name', 'avatar']);
+     
+     if(!profile)
+     {
+         return res.status(400).json({msg: "No profile found"});
+     }
+     return res.json(profile);
+    }
+    catch(err)
+    {
+        console.error(err.message);
+        return res.status(500).send("Server error");
+    }
     res.send("user route check");
+
+})
+
+//create or update
+
+router.post('/',[auth,[
+    check('status','Status is required').not().isEmpty(),
+    check('skills', 'Skills are Required').not().isEmpty()
+
+]] ,async(req,res)=>{
+   
+    
+        const errors = validationResult(req);
+        if(!errors.isEmpty())
+         {
+             return res.status(400).json({errors: errors.array()});
+         }
+
+         const {
+             company,
+             website,
+             location,
+             bio,
+             status,
+             githubusername,
+             skills,
+             youtube,
+             facebook,
+             twitter,
+             instagram,
+             linkedin
+         } = req.body;
+
+         const profileFields = {};
+         profileFields.user = req.user.id;
+         if(company) profileFields.company = company;
+         if(location) profileFields.location = location
+         if(website) profileFields.website = website;
+         if(bio) profileFields.bio = bio;
+         if(status) profileFields.status = status;
+         if(githubusername) profileFields.githubusername = githubusername;
+         
+         if(skills)
+         {
+             profileFields.skills = skills.split(',').map(skill =>skill.trim());
+         }
+
+         profileFields.social = {};
+         if(youtube) profileFields.social.youtube = youtube;
+         if(facebook) profileFields.social.facebook = facebook;
+         if(twitter) profileFields.social.twitter = twitter;
+         if(instagram) profileFields.social.instagram = instagram;
+         if(linkedin) profileFields.social.linkedin = linkedin;
+
+         console.log(profileFields);
+         //res.send("hello");
+         
+    try
+    {
+        let profile = await Profile.findOne({user:req.user.id});
+        //update
+        if(profile)
+        {
+            profile = await Profile.findOneAndUpdate(
+                {user: req.user.id},
+                {$set: profileFields},
+                {new: true}
+            )
+
+            return res.json(profile);
+        }
+
+        //create
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+
+    }
+    catch(err)
+    {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+    }
 })
 
 module.exports  = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const User = require('../../models/Users');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {check, validationResult} = require('express-validator/check');
+const {check, validationResult} = require('express-validator');
 const config = require('config');
 //add user route
 router.post('/',
