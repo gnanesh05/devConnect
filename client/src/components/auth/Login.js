8fba0f1dd91816d39dfb9aa225d2f5c1ba8ed896
gnanesh05 <gnanesh007.mk@gmail.com>
2021-07-16 00:30:13 +0530

    Added Register and Login forms

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,67 @@
+import { Fragment, useState } from "react"
+import axios from 'axios';
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+    const onSubmit = async e => {
+        e.preventDefault();
+        console.log(formData);
+        // const newuser = {
+        //     name,
+        //     email,
+        //     password
+        // };
+
+        // try {
+        //     const config = {
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         }
+        //     }
+        //     const body = JSON.stringify(newuser);
+        //     const res = await axios.post('/api/users', body, config);
+        //     console.log(res.data);
+        // }
+        // catch (err) {
+        //     console.error(err.response.data);
+        // }
+
+    }
     return (
-        <div>
-            Login
-        </div>
+        <Fragment>
+            <h1 class="large text-primary">Sign Up</h1>
+            <p class="lead"><i class="fas fa-user"></i> Sign in to Your Account</p>
+            <form class="form" onSubmit={e => onSubmit(e)}>
+                <div class="form-group">
+                    <input type="email" placeholder="Email Address" value={email} onChange={e => onChange(e)} name="email" required />
+                </div>
+                <div class="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        required
+                        value={password}
+                        onChange={e => onChange(e)}
+
+                    />
+                </div>
+
+                <input type="submit" className="btn btn-primary" value="Sign In" />
+            </form>
+            <p class="my-1">
+                Don't have an account? <Link to="/register">Register</Link>
+            </p>
+        </Fragment>
     )
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,89 @@
+import { Fragment, useState } from "react"
+import axios from 'axios';
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+    const onSubmit = async e => {
+        e.preventDefault();
+        if (password !== password2)
+            console.log("Passwords didn't match");
+        else {
+            console.log(formData);
+            // const newuser = {
+            //     name,
+            //     email,
+            //     password
+            // };
+
+            // try {
+            //     const config = {
+            //         headers: {
+            //             'Content-Type': 'application/json'
+            //         }
+            //     }
+            //     const body = JSON.stringify(newuser);
+            //     const res = await axios.post('/api/users', body, config);
+            //     console.log(res.data);
+            // }
+            // catch (err) {
+            //     console.error(err.response.data);
+            // }
+        }
+    }
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+            <h1 class="large text-primary">Sign Up</h1>
+            <p class="lead"><i class="fas fa-user"></i> Create Your Account</p>
+            <form class="form" onSubmit={e => onSubmit(e)}>
+                <div class="form-group">
+                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+                </div>
+                <div class="form-group">
+                    <input type="email" placeholder="Email Address" value={email} onChange={e => onChange(e)} name="email" required />
+                    <small class="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+                    >
+                </div>
+                <div class="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        required
+                        value={password}
+                        onChange={e => onChange(e)}
+
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        minLength="6"
+                        required
+                        value={password2}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p class="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
     )
 }
