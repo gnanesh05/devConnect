25ee667cfc1f66d9733d2b2d719209d513bda34e
gnanesh05 <gnanesh007.mk@gmail.com>
2021-07-23 22:02:48 +0530

    dashboard added

---
+++ client/src/actions/profile.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { setAlert } from "./alert";
+
+import { PROFILE_ERROR, GET_PROFILE } from "./Type";
+
+
+export const getCurrentProfile = () => async dispatch =>{
+    try {
+        const res = await axios.get('/api/profile/me');
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: error.response.statusText, status: error.response.status}
+        });
+    }
+}
